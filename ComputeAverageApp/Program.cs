75ce5804f374caaf461d89ef91361c5ee400caad
garcia.285082 <garcia.285082@LAB-A-PC10>
2024-02-23 07:55:41 +0800

    Add project files.

---
+++ ComputeAverageApp/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ComputeAverageApp
+{
+    class ComputeAverageProgram
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter 5 grades separated by new line: ");
+
+            double sum = 0;
+            int i = 0;
+
+            while (i < 5)
+            {
+                    
+                double grade = Convert.ToDouble(Console.ReadLine());
+                sum += grade;
+                i++;
+
+            }
+            double average = sum / 5;
+            double roundedAverage = Math.Round(average, 2);
+
+            Console.WriteLine("The average grade is " + average + " and rounded off to " + Math.Round(average));
+
+        }
+    }
+}
